c9208bf593751dd0a8b169b80c036f52aa76a700
Shiva Nadar <shivanesh_nadar@outlook.com>
2019-11-07 04:13:48 +0530

    Add project files.

---
+++ PopCorn/PopCorn/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PopCorn.Models;
+
+namespace PopCorn.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
